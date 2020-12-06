91a838976c043c6f73c168712c3de2a5fc63a1c5
Lenin Adin Hernández Vázquez <lennhv@gmail.com>
2020-12-06 10:30:29 -0600

    configure model in admin

--- exchangerate/exchanges/admin.py
+++ exchangerate/exchanges/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import CurrencyExchage
+
+
+@admin.register(CurrencyExchage)
+class CurrencyExchageManager(admin.ModelAdmin):
+    list_display = ('source', 'rate', 'last_update', 'date',)
