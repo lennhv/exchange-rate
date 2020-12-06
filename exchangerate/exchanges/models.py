dae5da5192f7c0801f0a7bc733cd2a7d82fb6893
Lenin Adin Hernández Vázquez <lennhv@gmail.com>
2020-12-06 08:28:04 -0600

    model for keep latest exchange rate

--- exchangerate/exchanges/models.py
+++ exchangerate/exchanges/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
+
+
+class CurrencyExchage(models.Model):
+    """ Currency exchange rate Model
+        Only keeps the latest rate by source and only for MXN to USD
+
+        To add another source, it is necessary to define it as another choice,
+         as well as those already defined.
+    """
+    BMX = 1
+    BANXICO = 2
+    FIXER = 3
+
+    # source options
+    SOURCES = (
+        (BMX, _('BOLSA MEXICANA')),
+        (BANXICO, _('BANXICO')),
+        (FIXER, _('FIXER')),
+    )
+
+    source = models.SmallIntegerField(_('source'), choices=SOURCES)
+    rate = models.FloatField(_('rate'),)
+    date = models.DateField(_('date'),)
+    last_update = models.DateTimeField(_('last_update'),)
+
+    class Meta:
+        verbose_name = _('currency exchange')
+        verbose_name_plural = _('currencies exchange')
+
+    def __str__(self):
+        return "%s %s" % (self.source, self.rate)
